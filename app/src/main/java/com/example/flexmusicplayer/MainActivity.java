f6bdb18ea53a94fd10cacd7acbffa7805ecf360f
uniuqex-x <1282966137@qq.com>
2026-03-04 15:21:56 +0800

    feat: android project init

---
+++ app/src/main/java/com/example/flexmusicplayer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.flexmusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.flexmusicplayer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'flexmusicplayer' library on application startup.
+    static {
+        System.loadLibrary("flexmusicplayer");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'flexmusicplayer' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
